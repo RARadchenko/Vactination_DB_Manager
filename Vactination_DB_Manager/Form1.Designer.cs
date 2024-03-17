2b14a3f07ebec3f1c9f5956688ce235ba6ab106e
Ruslan Radchenko <r.a.radchenko@student.csn.khai.edu>
2024-03-17 18:16:55 +0200

    Added current date and time in status bar

--- Vactination_DB_Manager/Form1.Designer.cs
+++ Vactination_DB_Manager/Form1.Designer.cs
@@ -29,12 +29,53 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.StatusTimeTimer = new System.Windows.Forms.Timer(this.components);
+            this.CurrentDate = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.CurrentDate});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // StatusTimeTimer
+            // 
+            this.StatusTimeTimer.Tick += new System.EventHandler(this.StatusTimeTimer_Tick);
+            // 
+            // CurrentDate
+            // 
+            this.CurrentDate.Name = "CurrentDate";
+            this.CurrentDate.Size = new System.Drawing.Size(88, 17);
+            this.CurrentDate.Text = "01.01.2024 0:0:1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.Timer StatusTimeTimer;
+        private System.Windows.Forms.ToolStripStatusLabel CurrentDate;
     }
 }
 
--- Vactination_DB_Manager/Form1.cs
+++ Vactination_DB_Manager/Form1.cs
@@ -16,5 +16,21 @@ namespace Vactination_DB_Manager
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StatusTimeTimer.Enabled = true;
+            // щоб одразу показати дату-час
+            StatusTimeTimer.Interval = 100;
+            StatusTimeTimer.Start(); // запуск таймера
+        }
+
+        private void StatusTimeTimer_Tick(object sender, EventArgs e)
+        {
+            StatusTimeTimer.Interval = 1000;
+            DateTime dt = DateTime.Now;
+            // перетворення DateTime.Now на дату/час
+            CurrentDate.Text = dt.ToString();
+        }
     }
 }
